7bba54f9c07f32fd24d3e6fa2436ce230056ffa6
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-01 16:51:14 -0300

    adjust background

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,7 @@ module.exports = {
         username: `paulok1`,
       },
     },
+    `gatsby-plugin-modal-routing`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
