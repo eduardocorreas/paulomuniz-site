db9516e260bb8c9d6ac70f05c7785c481a0adfec
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-01 15:31:18 -0300

    adjust margin bottom portfolio

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -4,7 +4,7 @@ import Img from 'gatsby-image'
 export default function PortfolioItem(node) {
   console.log(node)
   return (
-    <div className="w-full sm:w-1/4">
+    <div className="w-full sm:w-1/4 mb-1">
       <Img fluid={node.image.childImageSharp.fluid} />
     </div>
   )
