821af874554ae425bd39a81f453fe0657a342c29
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-01 13:07:20 -0300

    adjust padding left and right

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,7 +3,7 @@ import PortfolioList from '../components/portfolioList'
 
 export default function Portfolio() {
   return (
-    <div className="sm:p-20">
+    <div className="sm:p-20 pr-5 pl-5">
       <div className="section-title">
         <h1 className="title">Portfolio</h1>
         <p className="subtitle">Fotografia e Filmagem</p>
